a817d13e2d2ebfef53bb9bcdfb614aca93ec7205
Boris Popov <popov@whitekefir.ru>
2024-12-12 13:23:04 +0300

    add impl for ctor for PortPin

---
+++ firmware/source/src/PortPin.cpp
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2024 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "PortPin.hpp"
+
+gric::PortPin::PortPin(PortName::name_t n, pin_t p):
+     port(n),
+     pin(p)
+{
+     return;
+}
