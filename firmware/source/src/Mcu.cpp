17bc73c1169bf678b12893bcff5f44d2de16512c
Boris Popov <popov@whitekefir.ru>
2024-12-12 16:47:30 +0300

    begin impl for Gpio::init

--- firmware/source/src/Mcu.cpp
+++ firmware/source/src/Mcu.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Mcu.hpp"
+#include "Gpio.hpp"
 
 #ifndef UNITTEST
 #include "ch32x035.h"
@@ -19,21 +20,10 @@ void gric::Mcu::init() const {
 
 void gric::Mcu::init(const PortPin& pp, const PinConf& pc) {
      if (pct.is_off(pp.port)) pct.on(pp.port);
-     //
-     // TODO
-     //
+     Gpio gpio;
+     gpio.init(pp, pc);
 }
-/*
-void GPIO_Toggle_INIT(void) {
-    GPIO_InitTypeDef GPIO_InitStructure = { 0 };
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
-}
-*/
 #ifdef    UNITTEST
 
 void gric::Mcu::nvic_config() const {
