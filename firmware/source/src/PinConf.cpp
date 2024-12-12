54b8baaa4f33a12aaf9be22b09927db5f57260c6
Boris Popov <popov@whitekefir.ru>
2024-12-12 13:02:06 +0300

    add impl for ctor for PinConf

---
+++ firmware/source/src/PinConf.cpp
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2024 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "PinConf.hpp"
+
+gric::PinConf::PinConf(PinDir::dir_t d, PinMode::mode_t m):
+     dir(d),
+     mode(m)
+{
+     return;
+}
