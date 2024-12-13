fe2fd46638c37ee42225f0517a9356e43f4e4384
Boris Popov <popov@whitekefir.ru>
2024-12-13 16:28:58 +0300

    add impl for GpioModeCalc::get_out

--- firmware/source/src/GpioModeCalc.cpp
+++ firmware/source/src/GpioModeCalc.cpp
@@ -17,18 +17,52 @@ GPIOMode_TypeDef gric::GpioModeCalc::get(const PinConf& pc) const {
 	  return get_out(pc.mode);
      }
 }
-/*
 
+GPIOMode_TypeDef gric::GpioModeCalc::get_out(PinMode::mode_t v) const {
+     switch (v) {
+
+     case PinMode::PushPull:
+	  return GPIO_Mode_Out_PP;
+
+     case PinMode::Alter:
+	  return GPIO_Mode_AF_PP;
+
+     default:
+	  return GPIO_Mode_Out_PP;
+     }
+}
+
+/*
 typedef enum
 {
     GPIO_Mode_AIN = 0x0,
     GPIO_Mode_IN_FLOATING = 0x04,
     GPIO_Mode_IPD = 0x28,
     GPIO_Mode_IPU = 0x48,
-    GPIO_Mode_Out_PP = 0x10,
-    GPIO_Mode_AF_PP = 0x18
+    * GPIO_Mode_Out_PP = 0x10,
+    * GPIO_Mode_AF_PP = 0x18
 } GPIOMode_TypeDef;
 
+     switch (v) {
+
+     case PinMode::PushPull:
+	  return GPIO_Mode_Out_PP;
+
+     case PinMode::Float:
+	  break;
+
+     case PinMode::PullUp:
+	  break;
+
+     case PinMode::PullDown:
+	  break;
+
+     case PinMode::Analog:
+	  break;
+
+     case PinMode::Alter:
+	  return GPIO_Mode_AF_PP;
+     }
 
 
  */
