038694455d0ac2f08693eaebf5f856fcdb9ef8c4
Boris Popov <popov@whitekefir.ru>
2024-12-16 16:41:47 +0300

    cleanup main

--- firmware/source/src/main.cpp
+++ firmware/source/src/main.cpp
@@ -7,30 +7,12 @@ int main() {
      Delayer dl;
      PrintConf pc(115200);
      mcu.init(pc);
-     // PortPin ppp(PortName::A, 0);
-     // PinConf ppc(PinDir::Out, PinMode::PushPull);
-     // mcu.init(ppp, ppc);
      LedConf lc(PortName::A, 0);
      mcu.init(lc);
+     auto led = mcu.get(lc);
 
-
-     /*
-     u8 i = 0;
-
-     * NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-     * SystemCoreClockUpdate();
-     * Delay_Init();
-     * USART_Printf_Init(115200);
-     * printf("SystemClk:%d\r\n", SystemCoreClock);
-     * printf("ChipID:%08x\r\n", DBGMCU_GetCHIPID());
-     * printf("GPIO Toggle TEST\r\n");
-     * GPIO_Toggle_INIT();
-
-     while (1) {
-	  Delay_Ms(500);
-	  GPIO_WriteBit(GPIOA,
-			GPIO_Pin_0,
-			(BitAction)((i == 0) ? (i = Bit_SET) : (i = Bit_RESET)));
+     while (true) {
+	  dl.ms(500);
+	  led.toggle();
      }
-     */
 }
