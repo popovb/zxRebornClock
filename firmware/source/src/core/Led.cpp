357e94471ca45ce705762957daf9421ca5f90e12
Boris Popov <popov@whitekefir.ru>
2024-12-16 15:57:07 +0300

    add impl for Led::off

--- firmware/source/src/core/Led.cpp
+++ firmware/source/src/core/Led.cpp
@@ -6,18 +6,20 @@
 #include "Gpio.hpp"
 
 void gric::Led::toggle() {
-     if (emit) {
+     if (emit)
 	  off();
-	  emit = false;
-
-     } else {
+     else
 	  on();
-	  emit = true;
-     }
 }
 
 void gric::Led::on() {
      Gpio gpio;
      gpio.write_bit(port, pin, false);
      emit = true;
+}
+
+void gric::Led::off() {
+     Gpio gpio;
+     gpio.write_bit(port, pin, true);
+     emit = false;
 }
