972fbb0b70bb667829005ce0f12bc3370a885db0
Boris Popov <popov@whitekefir.ru>
2024-12-21 16:14:36 +0300

    add impl for Pin::write

--- firmware/source/src/core/Pin.cpp
+++ firmware/source/src/core/Pin.cpp
@@ -4,26 +4,12 @@
 
 #include "core/Pin.hpp"
 #include "Gpio.hpp"
-/*
-void gric::Pin::toggle() {
-     if (emit)
-	  off();
-     else
-	  on();
+
+void gric::Pin::write(bool v) const {
+     Gpio gpio;
+     gpio.write_bit(port, pin, v);
 }
 
-void gric::Pin::on() {
-     Gpio gpio;
-     gpio.write_bit(port, pin, false);
-     emit = true;
-}
-
-void gric::Pin::off() {
-     Gpio gpio;
-     gpio.write_bit(port, pin, true);
-     emit = false;
-}
-*/
 gric::Pin::Pin(u32 p, u32 i):
      port(p),
      pin(i)
