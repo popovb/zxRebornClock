c3c3c50002d3f67a0cf388f1708961bc11929994
Boris Popov <popov@whitekefir.ru>
2024-12-26 10:09:58 +0300

    fix impl for Tubes

--- firmware/source/src/Tubes.cpp
+++ firmware/source/src/Tubes.cpp
@@ -4,14 +4,14 @@
 
 #include "Tubes.hpp"
 
-gri::Tubes::Tubes(const Anodes& v1, const Cathodes& v2):
+gric::Tubes::Tubes(const Anodes& v1, const Cathodes& v2):
      as(v1),
      cs(v2)
 {
      return;
 }
 
-void gri::Tubes::display(u8 v[4]) const {
+void gric::Tubes::display(u8 v[4]) const {
      display_1(v);
      delay.ms(display_time_ms);
      blank();
@@ -33,27 +33,27 @@ void gri::Tubes::display(u8 v[4]) const {
      delay.us(blank_time_us);
 }
 
-void gri::Tubes::blank() const {
+void gric::Tubes::blank() const {
      as.clear();
      cs.clear();
 }
 
-void gri::Tubes::display_1(u8 v[4]) const {
+void gric::Tubes::display_1(u8 v[4]) const {
      cs.set(v[3]);
      as.set(1);
 }
 
-void gri::Tubes::display_2(u8 v[4]) const {
+void gric::Tubes::display_2(u8 v[4]) const {
      cs.set(v[2]);
      as.set(2);
 }
 
-void gri::Tubes::display_3(u8 v[4]) const {
+void gric::Tubes::display_3(u8 v[4]) const {
      cs.set(v[1]);
      as.set(3);
 }
 
-void gri::Tubes::display_4(u8 v[4]) const {
+void gric::Tubes::display_4(u8 v[4]) const {
      cs.set(v[0]);
      as.set(4);
 }
