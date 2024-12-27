f6045a5fe13b08985e635932e1348d172c2c25bf
Boris Popov <popov@whitekefir.ru>
2024-12-27 13:41:10 +0300

    add impl for Time::operator bool

--- firmware/source/src/Time.cpp
+++ firmware/source/src/Time.cpp
@@ -10,3 +10,11 @@ gric::Time::Time():
 {
      return;
 }
+
+gric::Time::operator bool() const {
+     if (minute < 0) return false;
+     if (hour < 0) return false;
+     if (minute > 59) return false;
+     if (hour > 23) return false;
+     return true;
+}
