200c391925011178496bfc28a2c3e4d971d56932
Boris Popov <popov@whitekefir.ru>
2024-12-28 12:15:57 +0300

    add impl for Rtc::pull

--- firmware/source/src/Rtc.cpp
+++ firmware/source/src/Rtc.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "Rtc.hpp"
+#include "ds1302/Hour.hpp"
+#include "ds1302/Minutes.hpp"
 
 gric::Rtc::Rtc(const Mcu& mcu, const McuNetConfig& cnf):
      ce(mcu, cnf),
@@ -11,4 +13,22 @@ gric::Rtc::Rtc(const Mcu& mcu, const McuNetConfig& cnf):
      transfer(ce, sclk, io, dln)
 {
      return;
+}
+
+gric::Time gric::Rtc::pull() const {
+     using namespace ds1302;
+     Hour hh;
+     Minutes mm;
+
+     Command h(Register::Hour,
+	       Target::Clock,
+	       Direction::Read);
+     Command m(Register::Minutes,
+	       Target::Clock,
+	       Direction::Read);
+
+     transfer.read(h, hh);
+     transfer.read(m, mm);
+
+     return Time(hh.get(), mm.get());
 }
