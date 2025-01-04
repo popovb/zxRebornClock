dbc2ac5b3f2aeaf5b4630b22d866a5e3c9297a7d
Boris Popov <popov@whitekefir.ru>
2025-01-04 13:19:02 +0300

    fix Anodes

--- firmware/source/src/Anodes.cpp
+++ firmware/source/src/Anodes.cpp
@@ -11,17 +11,18 @@ gric::Anodes::Anodes(const Mcu& m, const McuNetConfig& mnc) {
 
 void gric::Anodes::init(const Mcu& m, const McuNetConfig& mnc) {
      const auto& a1 = mnc.get(McuNet::SA1);
-     init(1, a1, m);
+     init(0, a1, m);
      const auto& a2 = mnc.get(McuNet::SA2);
-     init(2, a2, m);
+     init(1, a2, m);
      const auto& a3 = mnc.get(McuNet::SA3);
-     init(3, a3, m);
+     init(2, a3, m);
      const auto& a4 = mnc.get(McuNet::SA4);
-     init(4, a4, m);
+     init(3, a4, m);
 }
 
-void gric::Anodes::init(u8 i, const PortPinConf& ppc, const Mcu& mcu) {
-     pins[i - 1] = mcu.get(ppc);
+void gric::Anodes::init(u8 i, const PortPinConf& ppc,
+			const Mcu& mcu) {
+     pins[i] = mcu.get(ppc);
 }
 
 void gric::Anodes::clear() const {
