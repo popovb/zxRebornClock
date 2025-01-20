a5ac73e4b53122aa6011677fdbde2221627b77eb
Boris Popov <popov@whitekefir.ru>
2025-01-20 15:13:10 +0300

    add impl for UartConf

---
+++ firmware/source/src/core/UartConf.cpp
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2025 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "core/UartConf.hpp"
+
+gric::UartConf::UartConf(UartName::name_t a,
+			 u32 b,
+			 const PinTx& c,
+			 const PinRx& d):
+     name(a),
+     speed(b),
+     pin_tx(c),
+     pin_rx(d)
+{
+     return;
+}
