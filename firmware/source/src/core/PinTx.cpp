0743c0455bf4f46c0b533ba2c525b61164bcf743
Boris Popov <popov@whitekefir.ru>
2025-01-20 14:09:01 +0300

    add impl for PinTx

---
+++ firmware/source/src/core/PinTx.cpp
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2025 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "core/PinTx.hpp"
+
+gric::PinTx::PinTx(PortName::name_t a, pin_t b):
+     PortPinConf(a, b)
+{
+     return;
+}
