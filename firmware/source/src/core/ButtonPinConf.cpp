964cf8d7db02c565d39eb20ca6a2db545fb50449
Boris Popov <popov@whitekefir.ru>
2025-05-29 10:44:54 +0300

    add impl for ctor for ButtonPinConf

---
+++ firmware/source/src/core/ButtonPinConf.cpp
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2025 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "ButtonPinConf.hpp"
+
+gric::ButtonPinConf::ButtonPinConf():
+     PinConf(PinDir::In, PinMode::Float)
+{
+     return;
+}
