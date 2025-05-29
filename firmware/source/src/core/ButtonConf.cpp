3b8afaf0caf662da866b74d1b88b8e0b9ff87743
Boris Popov <popov@whitekefir.ru>
2025-05-29 10:48:41 +0300

    add impl for ctor for ButtonConf

---
+++ firmware/source/src/core/ButtonConf.cpp
@@ -0,0 +1,12 @@
+//
+// Copyright (c) 2025 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "core/ButtonConf.hpp"
+
+gric::ButtonConf::ButtonConf(PortName::name_t n, pin_t p):
+     port(n),
+     pin(p)
+{
+     return;
+}
