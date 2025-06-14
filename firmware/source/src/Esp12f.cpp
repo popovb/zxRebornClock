c250b0b08a1c56dd938cd66b49b6d55307e6b475
Boris Popov <popov@whitekefir.ru>
2025-06-14 14:24:35 +0300

    add impl for Esp12f::enable

--- firmware/source/src/Esp12f.cpp
+++ firmware/source/src/Esp12f.cpp
@@ -9,6 +9,10 @@ gric::Esp12f::Esp12f(const Mcu& m, const McuNetConfig& mnc) {
      init_uart(m, mnc);
 }
 
+void gric::Esp12f::enable() const {
+     chip_select.write(true);
+}
+
 void gric::Esp12f::
 init_pin(const Mcu& m, const McuNetConfig& mnc) {
      const PortPinConf& ppc = mnc.get(McuNet::ESP_EN);
