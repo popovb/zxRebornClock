21cde2028244542c6c339a5f57e844283e16218f
Boris Popov <popov@whitekefir.ru>
2025-06-14 14:15:14 +0300

    add impl for Esp12f::init_pin

--- firmware/source/src/Esp12f.cpp
+++ firmware/source/src/Esp12f.cpp
@@ -8,3 +8,9 @@ gric::Esp12f::Esp12f(const Mcu& m, const McuNetConfig& mnc) {
      init_pin(m, mnc);
      init_uart(m, mnc);
 }
+
+void gric::Esp12f::
+init_pin(const Mcu& m, const McuNetConfig& mnc) {
+     const PortPinConf& ppc = mnc.get(McuNet::ESP_EN);
+     chip_select = m.get(PortPinConf(ppc.port, ppc.pin));
+}
