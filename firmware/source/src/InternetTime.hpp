a7d3b21a28ae749f9719721672e05cec484c740f
Boris Popov <popov@whitekefir.ru>
2025-06-21 17:00:00 +0300

    begin proto for InternetTime

---
+++ firmware/source/src/InternetTime.hpp
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2025 Boris Popov <popov@whitekefir.ru>
+//
+
+#include "Esp12f.hpp"
+
+#ifndef _GRIC_INTERNET_TIME_HPP_
+#define _GRIC_INTERNET_TIME_HPP_
+
+namespace gric {
+
+     class InternetTime {
+
+     public:
+	  InternetTime(const Esp12f&);
+     };
+}
+#endif // _GRIC_INTERNET_TIME_HPP_
--- firmware/source/src/main.cpp
+++ firmware/source/src/main.cpp
@@ -3,6 +3,89 @@
 //
 
 #include "Configurator.hpp"
+#include "McuNetConfig.hpp"
+#include "Anodes.hpp"
+#include "Cathodes.hpp"
+#include "Tubes.hpp"
+#include "Rtc.hpp"
+#include "Time.hpp"
+#include "TimeChecker.hpp"
+#include "LedBlockControl.hpp"
+#include "KeyBlock.hpp"
+#include "KeyBlockReactor.hpp"
+#include "Esp12f.hpp"
+#include "InternetTime.hpp"
+#include "core/Mcu.hpp"
+
+int main() {
+     using namespace gric;
+     Mcu mcu;
+     Configurator cnf(mcu);
+     McuNetConfig mnc;
+     cnf.init(mnc);
+
+     Anodes as(mcu, mnc);
+     Cathodes cs(mcu, mnc);
+     Tubes tb(as, cs);
+
+     DisplayTime dt = tb.get_times();
+     u16 iters = dt.iters_per_second();
+
+     Rtc rtc(mcu, mnc);
+
+     // TIME SET EXAMPLE
+     // Time tn(18, 30);
+     // rtc.set(tn);
+
+
+     LedBlock lb(mcu, mnc);
+     LedBlockControl lbc(dt.iter_time(), lb);
+
+     KeyBlock kb(mcu, mnc);
+     KeyBlockReactor kbr(lbc, rtc);
+
+     // LedTaskTime ltt_r(2000, 2000);
+     // LedTaskTime ltt_y(1000, 1000);
+     // LedTaskTime ltt_g(500, 500);
+     // LedTaskTime ltt_b(250, 250);
+
+     // LedTask lt0(LedTaskMode::Unlimit, ltt_r, 0);
+     // LedTask lt1(LedTaskMode::Unlimit, ltt_y, 0);
+     // LedTask lt2(LedTaskMode::Unlimit, ltt_g, 0);
+     // LedTask lt3(LedTaskMode::Unlimit, ltt_b, 0);
+
+     // lbc.set(LedColor::Red, lt0);
+     // lbc.set(LedColor::Yellow, lt1);
+     // lbc.set(LedColor::Green, lt2);
+     // lbc.set(LedColor::Blue, lt3);
+     
+     Time tm = rtc.pull();
+     TimeChecker tc;
+     tc.put(tm);
+     u8 v[4];
+
+     Esp12f esp(mcu, mnc);
+     InternetTime it(esp);
+
+     while (true) {
+	  tm = rtc.pull();
+	  tc.put(tm);
+	  if (tc)
+	       tc.fill(v);
+	  else
+	       tc.fill_prev(v);
+
+	  for (u16 i = 0; i < iters; i++) {
+	       tb.display(v);
+	       auto kbe = kb.poll();
+	       kbr.handle(kbe, v);
+	       lbc.poll();
+	  }
+     }
+     return 0;
+}
+/*
+#include "Configurator.hpp"
 #include "RedLed.hpp"
 #include "BlueLed.hpp"
 #include "Esp12f.hpp"
@@ -10,7 +93,7 @@
 #include "core/Mcu.hpp"
 #include "core/DelayerNop.hpp"
 #include "debug.h"
-
+  
 int main() {
      using namespace gric;
      Mcu mcu;
@@ -76,132 +159,9 @@ int main() {
 
      while (true) { ; };
 }
-
+*/
 /*
-int main() {
-     using namespace gric;
-     Mcu mcu;
-
-     PrintConf pc(115200);
-     mcu.init(pc);
-
-     Configurator cnf(mcu);
-     McuNetConfig mnc;
-     cnf.init(mnc);
-
-     Esp12f esp(mcu, mnc);
-     EspReceiveBuffer erb;
-     RedLed rl(mcu, mnc);
-     BlueLed bl(mcu, mnc);
-     rl.on();
-
-     using Delayer = DelayerNop<4'000'000>;
-     Delayer dl;
-
-     dl.ms(500);
-     rl.off();
-     dl.ms(500);
-
-     for (int i = 0; i < 3; i++) {
-	  ///////////////////////////////////////////////
-	  bl.on();
-	  esp.on();
-	  dl.ms(300);
-	  esp.uart_enable();
-
-	  esp.send(EspCommand::at);
-	  esp.receive(erb);
-	  printf("%s\r\n", erb.buffer);
-
-	  esp.send(EspCommand::uart_def);
-	  esp.receive(erb);
-	  printf("%s\r\n", erb.buffer);
-
-	  esp.uart_disable();
-	  esp.off();
-	  bl.off();
-	  dl.ms(1000);
-	  ///////////////////////////////////////////////
-	  }
-     while (true) { ; };
-}
 // work
-#include "Configurator.hpp"
-#include "McuNetConfig.hpp"
-#include "Anodes.hpp"
-#include "Cathodes.hpp"
-#include "Tubes.hpp"
-#include "Rtc.hpp"
-#include "Time.hpp"
-#include "TimeChecker.hpp"
-#include "LedBlockControl.hpp"
-#include "KeyBlock.hpp"
-#include "KeyBlockReactor.hpp"
-#include "core/Mcu.hpp"
-
-int main() {
-     using namespace gric;
-     Mcu mcu;
-     Configurator cnf(mcu);
-     McuNetConfig mnc;
-     cnf.init(mnc);
-
-     Anodes as(mcu, mnc);
-     Cathodes cs(mcu, mnc);
-     Tubes tb(as, cs);
-
-     DisplayTime dt = tb.get_times();
-     u16 iters = dt.iters_per_second();
-
-     Rtc rtc(mcu, mnc);
-     // TIME SET EXAMPLE
-     // Time tn(18, 30);
-     // rtc.set(tn);
-
-
-     LedBlock lb(mcu, mnc);
-     LedBlockControl lbc(dt.iter_time(), lb);
-
-     KeyBlock kb(mcu, mnc);
-     KeyBlockReactor kbr(lbc, rtc);
-
-     // LedTaskTime ltt_r(2000, 2000);
-     // LedTaskTime ltt_y(1000, 1000);
-     // LedTaskTime ltt_g(500, 500);
-     // LedTaskTime ltt_b(250, 250);
-
-     // LedTask lt0(LedTaskMode::Unlimit, ltt_r, 0);
-     // LedTask lt1(LedTaskMode::Unlimit, ltt_y, 0);
-     // LedTask lt2(LedTaskMode::Unlimit, ltt_g, 0);
-     // LedTask lt3(LedTaskMode::Unlimit, ltt_b, 0);
-
-     // lbc.set(LedColor::Red, lt0);
-     // lbc.set(LedColor::Yellow, lt1);
-     // lbc.set(LedColor::Green, lt2);
-     // lbc.set(LedColor::Blue, lt3);
-     
-     Time tm = rtc.pull();
-     TimeChecker tc;
-     tc.put(tm);
-     u8 v[4];
-
-     while (true) {
-	  tm = rtc.pull();
-	  tc.put(tm);
-	  if (tc)
-	       tc.fill(v);
-	  else
-	       tc.fill_prev(v);
-
-	  for (u16 i = 0; i < iters; i++) {
-	       tb.display(v);
-	       auto kbe = kb.poll();
-	       kbr.handle(kbe, v);
-	       lbc.poll();
-	  }
-     }
-     return 0;
-}
 */
 /*
 int main() {
