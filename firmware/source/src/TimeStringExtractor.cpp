d001610ebfe57b476a20f9e10b44477185986b6e
Boris Popov <popov@whitekefir.ru>
2025-06-22 11:09:54 +0300

    add impl for TimeStringExtractor::load_second

--- firmware/source/src/TimeStringExtractor.cpp
+++ firmware/source/src/TimeStringExtractor.cpp
@@ -15,10 +15,20 @@ const char* in = "AT+CIPSNTPTIME?\r\n\
 +CIPSNTPTIME:Thu Aug 04 14:48:05 2016\r\n\
 OK\r";
 */
+gric::u8
+gric::TimeStringExtractor::load_second(u8& i) const {
+     char s[3] = { '\0', };
+     u8 n = find_rev(i, ':');
+     i = n;
+     s[0] = str[i + 1];
+     s[1] = str[i + 2];
+     return atoi(s);
+}
+
 gric::u16
 gric::TimeStringExtractor::load_year(u8 cr, u8& i) const {
      char y[5];
-     u8 n = find_rev(i, ' ');
+     u8 n = find_rev(cr, ' ');
      i = n;
      y[0] = str[i + 1];
      y[1] = str[i + 2];
@@ -44,7 +54,7 @@ gric::u8 gric::TimeStringExtractor::find_cr_2() const {
 gric::u8 gric::TimeStringExtractor::
 find_rev(u8 i, char c) const {
      while (i != 0) {
-	  if (str[i] == 'c') return i;
+	  if (str[i] == c) return i;
 	  --i;
      }
      return 0;
