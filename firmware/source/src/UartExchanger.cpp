d1a8c0724cd0c64612f7e41e47c48d0d1cbd2121
Boris Popov <popov@whitekefir.ru>
2025-07-12 12:38:13 +0300

    add impl for UartExchanger::init_uart

--- firmware/source/src/UartExchanger.cpp
+++ firmware/source/src/UartExchanger.cpp
@@ -7,3 +7,9 @@
 gric::UartExchanger::UartExchanger(Mcu& v) {
      init_uart(v);
 }
+
+void gric::UartExchanger::init_uart(Mcu& m) {
+     UartConf uc(UartName::Uart1, 115200);
+     m.init(uc);
+     uart = m.get(UartName::Uart1);
+}
